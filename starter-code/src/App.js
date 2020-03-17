67a5d0bf96f38dfebe23062725b86b14d2f0b07a
Peacock-Co <pavaoleonardo@hotmail.com>
2020-03-17 19:34:54 +0100

    adding axios

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,13 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { CountryDetail } from "./components/CountryDetail";
+import axios from "axios";
 
-function App() {
+const App = props => {
+  const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+  const getCoutry = async number => {
+    const url = `https://raw.githubusercontent.com/mledoze/countries/master/countries`;
+    const response = await axios.get(url);
+    const { data } = response;
+    return {
+      flag: data.flag,
+      name: data.name
+    };
+  };
+
+  const [country, clickedCountry] = useState(false);
+
   return (
     <div className="App">
-      <CountryDetail />
+      <div>
+        {
+          <div>
+            <nav className="navbar navbar-dark bg-primary mb-3">
+              <div className="container">
+                <a className="navbar-brand" href="/">
+                  WikiCountries
+                </a>
+              </div>
+            </nav>
+            <div className="container">
+              <div className="row">
+                <div
+                  className="col-5"
+                  style={{ maxHeight: "90vh", overflow: "scroll" }}
+                >
+                  <div className="list-group">
+                    <a
+                      className="list-group-item list-group-item-action"
+                      href="/ABW"
+                      value={country[0]}
+                      onChange={event => clickedCountry[1]()}
+                    >
+                      <span>🇦🇼</span> Aruba
+                    </a>
+                    <a
+                      className="list-group-item list-group-item-action"
+                      href="/AFG"
+                    >
+                      <span>🇦🇫</span> Afghanistan
+                    </a>
+                  </div>
+                </div>
+                <CountryDetail />
+              </div>
+            </div>
+          </div>
+        }
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- starter-code/src/components/CountryDetail.js
+++ starter-code/src/components/CountryDetail.js
@@ -1,168 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const CountryDetail = () => {
+export const CountryDetail = props => {
   return (
-    <div>
+    <div className="col-6">
       {
         <div>
-          <nav className="navbar navbar-dark bg-primary mb-3">
-            <div className="container">
-              <a className="navbar-brand" href="/">
-                WikiCountries
-              </a>
-            </div>
-          </nav>
-          <div className="container">
-            <div className="row">
-              <div
-                className="col-5"
-                style={{ maxHeight: "90vh", overflow: "scroll" }}
-              >
-                <div className="list-group">
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/ABW"
-                  >
-                    <span>🇦🇼</span> Aruba
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/AFG"
-                  >
-                    <span>🇦🇫</span> Afghanistan
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/AGO"
-                  >
-                    <span>🇦🇴</span> Angola
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/AIA"
-                  >
-                    <span>🇦🇮</span> Anguilla
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/ALA"
-                  >
-                    <span>🇦🇽</span> Åland Islands
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/ALB"
-                  >
-                    <span>🇦🇱</span> Albania
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/AND"
-                  >
-                    <span>🇦🇩</span> Andorra
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/ARE"
-                  >
-                    <span>🇦🇪</span> United Arab Emirates
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/ARG"
-                  >
-                    <span>🇦🇷</span> Argentina
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/ARM"
-                  >
-                    <span>🇦🇲</span> Armenia
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/ASM"
-                  >
-                    <span>🇦🇸</span> American Samoa
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/ATA"
-                  >
-                    <span>🇦🇶</span> Antarctica
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/FLK"
-                  >
-                    <span>🇫🇰</span> Falkland Islands
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action active"
-                    href="/FRA"
-                  >
-                    <span>🇫🇷</span> France
-                  </a>
-                  <a
-                    className="list-group-item list-group-item-action"
-                    href="/ZWE"
-                  >
-                    <span>🇿🇼</span> Zimbabwe
-                  </a>
-                </div>
-              </div>
-              <div className="col-7">
-                <h1>France</h1>
-                <table className="table">
-                  <thead></thead>
-                  <tbody>
-                    <tr>
-                      <td style={{ width: "30%" }}>Capital</td>
-                      <td>Paris</td>
-                    </tr>
-                    <tr>
-                      <td>Area</td>
-                      <td>
-                        551695 km
-                        <sup>2</sup>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Borders</td>
-                      <td>
-                        <ul>
-                          <li>
-                            <a href="/AND">Andorra</a>
-                          </li>
-                          <li>
-                            <a href="/BEL">Belgium</a>
-                          </li>
-                          <li>
-                            <a href="/DEU">Germany</a>
-                          </li>
-                          <li>
-                            <a href="/ITA">Italy</a>
-                          </li>
-                          <li>
-                            <a href="/LUX">Luxembourg</a>
-                          </li>
-                          <li>
-                            <a href="/MCO">Monaco</a>
-                          </li>
-                          <li>
-                            <a href="/ESP">Spain</a>
-                          </li>
-                          <li>
-                            <a href="/CHE">Switzerland</a>
-                          </li>
-                        </ul>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
+          <h1>France</h1>
+          <table className="table">
+            <thead></thead>
+            <tbody>
+              <tr>
+                <td style={{ width: "50%" }}>Capital</td>
+                <td>Paris</td>
+              </tr>
+              <tr>
+                <td>Area</td>
+                <td>
+                  551695 km
+                  <sup>2</sup>
+                </td>
+              </tr>
+              <tr>
+                <td>Borders</td>
+                <td>
+                  <ul>
+                    <li>
+                      <a href="/AND">Andorra</a>
+                    </li>
+                    <li>
+                      <a href="/BEL">Belgium</a>
+                    </li>
+                    <li>
+                      <a href="/DEU">Germany</a>
+                    </li>
+                    <li>
+                      <a href="/ITA">Italy</a>
+                    </li>
+                    <li>
+                      <a href="/LUX">Luxembourg</a>
+                    </li>
+                    <li>
+                      <a href="/MCO">Monaco</a>
+                    </li>
+                    <li>
+                      <a href="/ESP">Spain</a>
+                    </li>
+                    <li>
+                      <a href="/CHE">Switzerland</a>
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       }
     </div>
